e7f7d2753261ce6eb98760c843737e893a0b83e1
Muniza .. <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-11-04 23:29:56 +0500

    task 04

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+     <>
+       <div className="footer">
+         <div className="iconsDiv">
+         <i className="bx bxl-facebook-square bx-sm"></i>
+         <i className="bx bxl-instagram bx-sm"></i>
+         <i className="bx bxl-twitter bx-sm"></i>
+         <i className="bx bxl-linkedin-square bx-sm"></i>
+         </div>
+         <p className="copyright">Copyright ©2020 All rights reserved</p>
+       </div>
+     </>
+    );
+  }
